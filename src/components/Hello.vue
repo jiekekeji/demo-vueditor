f493d0d0efd3eeaaedabac8fef298023616e3e8f
jiekekeji <jieke_rose@foxmail.com>
2017-08-10 10:53:25 +0800

    富文本编辑器-vueditor的使用例子

---
+++ src/components/Hello.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="hello">
+    <div id="editorContainer"></div>
+    <button @click="obtainContent">获取内容</button>
+    <div>
+      <h1 data-v-289543ab="" style="text-align: center;"><span data-v-289543ab="" style="font-size: 1.5rem;"><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">易淘鲜</span><span data-v-289543ab=""
+                                                                                            style="font-family: 宋体; color: rgb(51, 51, 51);">用户协议</span></span>
+      </h1>
+      <div><span data-v-289543ab="" style="font-size: 1.5rem;"><span data-v-289543ab=""
+                                                                     style="font-family: 宋体; color: rgb(51, 51, 51);"><div
+        data-v-289543ab="" style="color: rgb(0, 0, 0); font-family: &quot;Microsoft YaHei&quot;; font-size: medium;"><p
+        data-v-289543ab="" class="MsoNormal"><span data-v-289543ab="" style="font-size: 1rem;"><span data-v-289543ab=""
+                                                                                                     style="font-family: 宋体; color: rgb(51, 51, 51);">&nbsp; &nbsp;本协议是用户您与</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">易淘鲜</span><span data-v-289543ab=""
+                                                                                            style="font-family: 宋体; color: rgb(51, 51, 51);"><font
+        data-v-289543ab="" face="宋体">（以下简称</font>"</span><span data-v-289543ab=""
+                                                               style="font-family: 宋体; color: rgb(51, 51, 51);">网络</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">平台</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">"，网址：）的所有者海南封神网络科技有限公司（以下简称"</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">甲方</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">"）之间</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">，</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">就</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">网络</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">平台</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">服务等相关事宜所订立的契约，请仔细阅读本注册协议。在您完成本网站注册，代表您已确认和接纳本网站的全部服务条款，本协议即构成对双方有约束力的法律文件。</span></span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51); font-size: 6pt;"><br
+        data-v-289543ab=""></span></p><h4 data-v-289543ab=""><b data-v-289543ab=""><span data-v-289543ab=""
+                                                                                         style="font-family: 宋体; color: rgb(51, 51, 51); font-size: 1.2rem;"><font
+        data-v-289543ab="" face="宋体">一、</font>&nbsp;<font data-v-289543ab="" face="宋体">用户注册</font></span></b></h4></div><div
+        data-v-289543ab="" style="color: rgb(0, 0, 0); font-family: &quot;Microsoft YaHei&quot;; font-size: medium;"><p
+        data-v-289543ab="" class="MsoNormal"><span data-v-289543ab="" style="font-size: 1rem;"><span data-v-289543ab=""
+                                                                                                     style="font-family: 宋体; color: rgb(51, 51, 51);">1. 用户注册是指用户登录</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">网络平台</span><span data-v-289543ab=""
+                                                                                             style="font-family: 宋体; color: rgb(51, 51, 51);">，按要求填写相关信息并确认同意履行相关用户协议的过程。用户因进行交易、获取有偿服务等而发生的所有应纳税赋由用户自行承担责任。</span></span></p><p
+        data-v-289543ab="" class="MsoNormal"><span data-v-289543ab="" style="font-size: 1rem;"><span data-v-289543ab=""
+                                                                                                     style="font-family: 宋体; color: rgb(51, 51, 51);">2. 本</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">网络平台</span><span data-v-289543ab=""
+                                                                                             style="font-family: 宋体; color: rgb(51, 51, 51);">用户必须是具有完全民事行为能力的自然人，或者是具</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">相关行业</span><span data-v-289543ab=""
+                                                                                             style="font-family: 宋体; color: rgb(51, 51, 51);">合法经营资格的</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">法人</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">。无民事行为能力人、限制民事行为能力人以及无经营或特定经营资格的组织不当注册为本站用户或超过其民事权利或行为能力范围从事与本站进行交易的，其与本站之间的服务协议自始无效，一经发现，</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">甲方</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">可</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">根据</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">本协议</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">立即</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">终止</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">与该用户的</span><span data-v-289543ab=""
+                                                                                              style="font-family: 宋体; color: rgb(51, 51, 51);">一切</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">往来</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">交易、</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">相关</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">账户</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">进行</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">注销</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">处理</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">甲方</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">并</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">有权</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);"><font
+        data-v-289543ab="" face="宋体">追究其使用本站</font>"服务"的一切法律责任。</span></span></p><p data-v-289543ab="" class="MsoNormal"
+                                                                                    style="text-align: center;"><span
+        data-v-289543ab="" style="font-size: 1rem;"><span data-v-289543ab=""
+                                                          style="font-family: 宋体; color: rgb(51, 51, 51);"><img
+        src="blob:http://localhost:8080/97b19ccc-00e8-4db2-b80a-06cfe7ea4956"><br></span></span></p><div
+        data-v-289543ab=""><h4 data-v-289543ab="" style="text-align: center;"><b data-v-289543ab=""><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51); font-size: 1.2rem;"><font data-v-289543ab=""
+                                                                                                     face="宋体">二、</font>&nbsp;<font
+        data-v-289543ab="" face="宋体">商品交易</font></span></b></h4></div><p data-v-289543ab="" class="MsoNormal"><span
+        data-v-289543ab="" style="font-size: 1rem;"><span data-v-289543ab=""
+                                                          style="font-family: 宋体; color: rgb(51, 51, 51);"></span></span></p><div
+        data-v-289543ab=""><p data-v-289543ab="" class="MsoNormal"><span data-v-289543ab=""
+                                                                         style="font-size: 1rem;"><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">用户在本</span><span data-v-289543ab=""
+                                                                                             style="font-family: 宋体; color: rgb(51, 51, 51);">网络平台</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">购买商品时必须遵守以下条款：</span></span></p><p
+        data-v-289543ab="" class="MsoNormal"><span data-v-289543ab="" style="font-size: 1rem;"><span data-v-289543ab=""
+                                                                                                     style="font-family: 宋体; color: rgb(51, 51, 51);">1. 用户在使用本</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">网络平台</span><span data-v-289543ab=""
+                                                                                             style="font-family: 宋体; color: rgb(51, 51, 51);">服务时应遵守所有适用的中国法律、法规、条例和地方性法律的要求。用户还必须确保遵守本协议及纳入本协议的所有其他条款和规则的规定。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);"><br data-v-289543ab=""></span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">2. 如果用户在本</span><span data-v-289543ab=""
+                                                                                                  style="font-family: 宋体; color: rgb(51, 51, 51);">网络平台</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">购买商品，用户有义务完成与本站的交易（法律或本协议禁止的交易除外）。通过对一项商品所下的订单，用户同意受该商品描述所含的出售条件的约束，该出售条件应不违反法律或本协议规定。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);"><br data-v-289543ab=""></span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">3. 用户通过本站订购商品，订单即为购买商品的申请或要约。但只有直接将商品发送至用户指定地址时，才构成本站对该订单的承诺，此时合同即告成立。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);"><br data-v-289543ab=""></span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">4.&nbsp;</span><span data-v-289543ab=""
+                                                                                                 style="font-family: 宋体; color: rgb(51, 51, 51);">甲方</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">有权在发现</span><span data-v-289543ab=""
+                                                                                              style="font-family: 宋体; color: rgb(51, 51, 51);">网络平台</span><span
+        data-v-289543ab=""
+        style="font-family: 宋体; color: rgb(51, 51, 51);">上显示的商品及订单明显错误或缺货的情况下，单方面撤回这些信息该订单。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">甲方</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">保留对商品订购数量的限制权。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);"><br data-v-289543ab=""></span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">5. 商品价格和可获性</span><span data-v-289543ab=""
+                                                                                                    style="font-family: 宋体; color: rgb(51, 51, 51);">都将在本站上标明</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">。</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">全部</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">价格</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">，</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">均需</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">包含</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">销项</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">增值税</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">的</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">计提</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">（按照法定税率）</span><span data-v-289543ab=""
+                                                                                                 style="font-family: 宋体; color: rgb(51, 51, 51);">，</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">唯</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">运输</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">费用</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">须</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">另外结算</span><span data-v-289543ab=""
+                                                                                             style="font-family: 宋体; color: rgb(51, 51, 51);">。</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">运输</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">费用</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">的</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">核算，</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">应</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">根据用户选择的送货方式的不同而</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">有</span><span data-v-289543ab=""
+                                                                                          style="font-family: 宋体; color: rgb(51, 51, 51);">差</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">异。</span><span data-v-289543ab=""
+                                                                                           style="font-family: 宋体; color: rgb(51, 51, 51);">甲方</span><span
+        data-v-289543ab="" style="font-family: 宋体; color: rgb(51, 51, 51);">有权随时更改这些信息而不做任何通知</span><span
+        data-v-289543ab=""
+        style="font-family: 宋体; color: rgb(51, 51, 51);">。</span></span></p></div></div></span></span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {createEditor} from 'vueditor'
+  import 'vueditor/dist/style/vueditor.min.css'
+  import lang from "vueditor/dist/language/lang.cn.js"
+  export default {
+    data () {
+      return {
+        inst: {},
+        content: ""
+      }
+    },
+    component: {},
+    activated(){
+      this.inst = createEditor('#editorContainer', {
+        toolbar: [
+          'removeFormat', 'undo', 'redo', '|', 'element', 'fontName', 'fontSize', 'foreColor', 'backColor', 'divider', 'bold', 'italic', 'underline', 'strikeThrough',
+          'link', 'unLink', 'divider', 'subscript', 'superscript', 'divider', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull',
+          '|', 'indent', 'outdent', 'insertOrderedList', 'insertUnorderedList', '|', 'emoji', 'picture', 'table', '|', 'fullscreen', 'sourceCode', 'markdown'
+        ],
+        fontName: [
+          {val: 'arial black'},
+          {val: 'times new roman'},
+          {val: 'Courier New'},
+          {val: '微软雅黑'}
+        ],
+        fontSize: ['12px', '14px', '16px', '18px', '0.8rem', '1.0rem', '1.2rem', '1.5rem', '2.0rem'],
+        uploadUrl: '',
+        id: '',
+        classList: ["editor-eara"],
+      });
+    },
+    methods: {
+      obtainContent(){
+        this.content = this.inst.getContent();
+        console.log(this.inst.getContent());
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+<style>
+  .editor-eara {
+    height: 500px;
+    width: 600px;
+    margin: 0 auto;
+  }
+</style>
